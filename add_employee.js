246b704647f3127acf630d16e046891c87aefcbc
Jason Savoie <flimits@gmail.com>
2023-10-30 23:07:32 -0700

    meant to save. tried to combine add_employee.js to prompt because the display was messing up. thea add employ function by itself works great, just not with the main funciton called prompt.js

--- add_employee.js
+++ add_employee.js
@@ -1,50 +1,54 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql2/promise');
 
-
-
-// Function to retreive data from companyzyx
-async function getDataFromDatabase() {
-
+// Function to create and return a database connection
+async function createDatabaseConnection() {
   const connection = await mysql.createConnection({
-
     host: '127.0.0.1',
     user: 'root',
     password: 'password',
     database: 'companyzyx',
+  });
 
-  })
-
-  const [myRoles] = await connection.execute('SELECT id,title FROM roles');
-  const [myMgrs] = await connection.execute('SELECT id,first_name,last_name FROM employee');
-  connection.end();
-  return {
-    myRoles,
-    myMgrs
-  };
+  return connection;
 }
 
-// Function to dispay choice to get the role for an Employee
+
 async function addEmployee() {
-
-  const { gotRolesFromDB,gotMgrsFromDB }  = await getDataFromDatabase();
-
-  const roleChoices = gotRolesFromDB.map(item => ({
+  const connection = await createDatabaseConnection();
+  const { myRoles, myMgrs } = await getDataFromDatabase(connection);
+  
+  const roleChoices = myRoles.map(item => ({
     name: item.title,
     value: item.id,
   }));
-
-  const mgrChoices = gotRolesFromDB.map(item => ({
+  
+  const mgrChoices = myMgrs.map(item => ({
     name: item.first_name + " " + item.last_name,
     value: item.id,
   }));
-
+  
+  // Function to retrieve data from the companyzyx database tables roles 
+  async function getDataFromDatabase(connection) {
+    const [myRoles] = await connection.execute('SELECT id, title FROM roles');
+    const [myMgrs] = await connection.execute('SELECT id, first_name, last_name FROM employee');
+    return {
+      myRoles,
+      myMgrs
+    };
+  }
+  
+  // Function to insert an employee into the database
+  async function insertEmployee(connection, data) {
+    const sql = 'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)';
+    await connection.execute(sql, [data.first_name, data.last_name, data.role_id, data.mgr_id]);
+  }
 
   inquirer
-    .prompt([
-      {
-        name: "first_name",
-        message: "Enter the Employee first name",
+  .prompt([
+    {
+      name: "first_name",
+      message: "Enter the Employee first name",
         type: "input",
       },
       {
@@ -53,19 +57,35 @@ async function addEmployee() {
         type: "input",
       },
       {
-        name: "role",
-        message: "What is your role?",
+        name: "role_id",
+        message: "What is the employee's role?",
         type: "list",
-        roleChoices,
+        choices: roleChoices,
+      },
+      {
+        name: "mgr_id",
+        message: "Who is the Manager for this new Employee?",
+        type: "list",
+        choices: mgrChoices,
       }
     ])
     .then(answers => {
-      console.log(answers)
-      console.log('selected ID: ' + answers.role);
-      console.log('User first and last name' + answers.first_name + answers.last_name);
+      console.log(answers);
+      console.log('Selected role ID: ' + answers.role_id);
+      console.log('User first and last name: ' + answers.first_name + " " + answers.last_name);
+      console.log('Manager ID: ' + answers.mgr_id);
 
+      insertEmployee(connection, answers)
+        .then(() => {
+          console.log('Employee inserted successfully.');
+          connection.end(); // Close the database connection
+        })
+        .catch(error => {
+          console.error('Error inserting employee:', error);
+          connection.end(); // Close the database connection even in case of an error
+        });
     });
-};
+}
 
 addEmployee(); // Start the initial prompt
 
--- prompt.js
+++ prompt.js
@@ -1,128 +1,170 @@
 const express = require('express');
 const inquirer = require('inquirer');
 const mysql = require('mysql2/promise');
-const addEmployee = require('./add_employee')
 
-// Connect to database
-const db = mysql.createConnection(
-    {
-        host: '127.0.0.1',
-        user: 'root',
-        password: 'password',
-        database: 'companyzyx'
-    },
-    console.log(`Connected to the companyzyx database.`)
-);
+const start = async () => {
+  // Create a connection to the database
+  const connection = await mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'password',
+    database: 'companyzyx'
+  });
 
+  console.log('Connected to the companyzyx database.');
 
-const runThroughChoices = function () {
-    const listOChoices = [
-        "View All Employees",
-        "Add Employee",
-        "View All Roles",
-        "Update Employee Role",
-        "View ALL Departments",
-        "Add Department",
-        "Exit"
+  // Function to display a list of choices
+  const runThroughChoices = async () => {
+    const choices = [
+      "View All Employees",
+      "Add Employee",
+      "View All Roles",
+      "Update Employee Role",
+      "View All Departments",
+      "Add Department",
+      "Exit"
     ];
 
-    function promptUser() {
-        inquirer
-            .prompt([
-                {
-                    name: "todoChoice",
-                    message: "What would you like to do?",
-                    type: "list",
-                    choices: listOChoices,
-                }
-            ])
-            .then(answers => {
-                switch (answers.todoChoice) {
-                    case "View All Employees":
-                        viewAllEmployees();
-                        break;
-                    case "Add Employee":
-                        addEmployee();
-                        break;
-                    case "Update Employee Role":
-                        updateEmployeeRole();
-                        break;
-                    case "View All Roles":
-                        viewAllRoles();
-                        break;
-                    case "View ALL Departments":
-                        viewAllDepartments();
-                        break;
-                    case "Add Department":
-                        addDepartment();
-                        break;
-                    case "Exit":
-                        console.log("You asked to leave. Have a good day.");
-                        break;
-                    default:
-                        console.log("Invalid choice. Please try again.");
-                        promptUser(); // Allow the user to try again
-                }
-            });
+    while (true) {
+      const { choice } = await inquirer.prompt({
+        name: "choice",
+        message: "What would you like to do?",
+        type: "list",
+        choices: choices
+      });
+
+      switch (choice) {
+        case "View All Employees":
+          await viewAllEmployees();
+          break;
+        case "Add Employee":
+          await addEmployee(connection); // Pass the connection object
+          break;
+        case "Update Employee Role":
+          await updateEmployeeRole();
+          break;
+        case "View All Roles":
+          await viewAllRoles();
+          break;
+        case "View All Departments":
+          await viewAllDepartments();
+          break;
+        case "Add Department":
+          await addDepartment();
+          break;
+        case "Exit":
+          console.log("You asked to leave. Have a good day.");
+          return; // Exit the loop and the program
+        default:
+          console.log("Invalid choice. Please try again.");
+      }
+    }
+  };
+
+  const viewAllEmployees = async () => {
+    // Query database and gather all Employees
+    const [rows] = await connection.execute('SELECT * FROM employee');
+    console.table(rows);
+  };
+
+  const addEmployee = async (connection) => {
+    const { myRoles, myMgrs } = await getDataFromDatabase(connection);
+
+    const roleChoices = myRoles.map(item => ({
+      name: item.title,
+      value: item.id,
+    }));
+
+    const mgrChoices = myMgrs.map(item => ({
+      name: item.first_name + " " + item.last_name,
+      value: item.id,
+    }));
+
+    // Function to retrieve data from the companyzyx database tables roles
+    async function getDataFromDatabase(connection) {
+      const [myRoles] = await connection.execute('SELECT id, title FROM roles');
+      const [myMgrs] = await connection.execute('SELECT id, first_name, last_name FROM employee');
+      return {
+        myRoles,
+        myMgrs
+      };
     }
 
-    promptUser(); // Start the initial prompt
-};
+    // Function to insert an employee into the database
+    async function insertEmployee(connection, data) {
+      const sql = 'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)';
+      await connection.execute(sql, [data.first_name, data.last_name, data.role_id, data.mgr_id]);
+    }
 
-const viewAllEmployees = function () {
-    // Query database and gather al Employees
-    db.query('SELECT * FROM employee', function (err, results) {
-        if (err) {
-            console.error(err);
-        } else {
-            console.table(results);
+    inquirer
+      .prompt([
+        {
+          name: "first_name",
+          message: "Enter the Employee first name",
+          type: "input",
+        },
+        {
+          name: "last_name",
+          message: "Enter the Employee last name",
+          type: "input",
+        },
+        {
+          name: "role_id",
+          message: "What is the employee's role?",
+          type: "list",
+          choices: roleChoices,
+        },
+        {
+          name: "mgr_id",
+          message: "Who is the Manager for this new Employee?",
+          type: "list",
+          choices: mgrChoices,
         }
-        // After the query is complete, go back to the menu prompt
-        runThroughChoices();
-    });
-};
+      ])
+      .then(answers => {
+        console.log(answers);
+        console.log('Selected role ID: ' + answers.role_id);
+        console.log('User first and last name: ' + answers.first_name + " " + answers.last_name);
+        console.log('Manager ID: ' + answers.mgr_id);
 
-// const addEmployee = function () {
-//     console.log("Adding Employee Here.");
-// };
+        insertEmployee(connection, answers)
+          .then(() => {
+            console.log('Employee inserted successfully.');
+            connection.end(); // Close the database connection
+          })
+          .catch(error => {
+            console.error('Error inserting employee:', error);
+            connection.end(); // Close the database connection even in case of an error
+          });
+      });
+  }
 
-
-const viewAllRoles = function () {
+  const viewAllRoles = async () => {
     console.log("Viewing all Roles Here.");
     // Query database for all Roles
-    db.query('SELECT * FROM roles', function (err, results) {
-        if (err) {
-            console.error(err);
-        } else {
-            console.table(results);
-        }
-        // After the query is complete, go back to the menu prompt
-        runThroughChoices();
-    });
-};
+    const [rows] = await connection.execute('SELECT * FROM roles');
+    console.table(rows);
+  };
 
-const updateEmployeeRole = function () {
+  const updateEmployeeRole = async () => {
     console.log("Updating Employee Role Here.");
-};
+    // Implement the logic to update an employee's role
+  };
 
-const viewAllDepartments = function () {
+  const viewAllDepartments = async () => {
     console.log("Viewing All Departments in Companyzyx.");
-    // Query database using a parameterized query
-    const sql = 'SELECT * FROM department';
-    db.query(sql, function (err, results) {
-        if (err) {
-            console.error(err);
-        } else {
-            console.table(results);
-        }
-        // After the query is complete, go back to the menu prompt
-        runThroughChoices();
-    });
-};
+    // Query database for all Departments
+    const [rows] = await connection.execute('SELECT * FROM department');
+    console.table(rows);
+  };
 
-const addDepartment = function () {
+  const addDepartment = async () => {
     console.log("Adding Department Here.");
-    runThroughChoices();
+    // Implement the logic to add a department
+  };
+
+  await runThroughChoices();
+  connection.end(); // Close the database connection when the program exits
 };
 
-runThroughChoices();
+start(); // Start the application
